3b7e903a18b664ad9dcf456739f98f3d787d9198
Abigail Cox <abigailgcox@outlook.com>
2023-03-21 12:32:23 +0000

    adds master URL to spark properties

--- data_processing.py
+++ data_processing.py
@@ -9,10 +9,10 @@ aws_credentials_file = "aws_config.yaml"
 # read data from S3 bucket with Spark
 # set environment variables (looks up configs from Maven Repo)
 os.environ["PYSPARK_SUBMIT_ARGS"] = "--packages com.amazonaws:aws-java-sdk-s3:1.12.430,org.apache.hadoop:hadoop-aws:3.3.4 pyspark-shell"
-config = SparkConf().setAppName("S3_to_Spark")
+config = SparkConf().setAppName("S3_to_Spark").setMaster("local[*]")
 
 sc = SparkContext(conf=config)
-spark = SparkSession(sc).builder.appName("S3_to_Spark").getOrCreate()
+spark = SparkSession(sc).builder.appName("S3_to_Spark").master("local[*]").getOrCreate()
 
 with open(aws_credentials_file, "r") as f:
     aws_creds = yaml.safe_load(f)
@@ -24,5 +24,5 @@ hadoopConf.set("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoo
 
 # create spark dataframe to read in files from the S3 bucket
 while True:
-    df = spark.read.json(f"s3a://pinterest-data-40dba6ba-1777-41cd-82ea-0ca36620f0b4/pins/pin_1000")
-    df.show(10, True)
+    df = spark.read.json(f"s3a://pinterest-data-40dba6ba-1777-41cd-82ea-0ca36620f0b4/posts/pin_10020.json")
+    df.show()
