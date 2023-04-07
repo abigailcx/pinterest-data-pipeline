6da02c91df6e66e4be45dd4065f3b4d397e116d3
Abigail Cox <abigailgcox@outlook.com>
2023-04-07 16:19:09 +0100

    Makes modifications to batch consumer and data processing

--- batch_consumer.py
+++ batch_consumer.py
@@ -2,14 +2,18 @@ from kafka import KafkaConsumer
 import boto3
 import json
 
-pinterest_s3_bucket = 'pinterest-data-40dba6ba-1777-41cd-82ea-0ca36620f0b4'
-
 topic = 'PinterestTopic1'
 bootstrap_servers = 'localhost:9092'
-value_deserializer = lambda m: json.loads(m.decode('ascii'))
+value_deserializer = lambda m: json.loads(m)
 batch_consumer = KafkaConsumer(topic, 
                                bootstrap_servers=bootstrap_servers, 
                                value_deserializer=value_deserializer)
+
+
+# create S3 resource
+s3_resource = boto3.resource('s3')
+pinterest_s3_bucket = 'pinterest-data-40dba6ba-1777-41cd-82ea-0ca36620f0b4'
+my_bucket = s3_resource.Bucket(pinterest_s3_bucket)
 
 for message in batch_consumer:
     print(message.topic)
@@ -17,19 +21,21 @@ for message in batch_consumer:
     print(message.value)
 
     # send data from kafka consumer to S3 bucket
-    # create S3 resource
-    s3 = boto3.client('s3')
-    # define tmp filepath on local machine to store json object
-    filename = f"pin_{message.value['index']}.json"
-    filepath = f"/Users/agc/AiCore/pinterest-data-pipeline/tmp/{filename}"
-    # convert message in batch consumer to json file
-    with open(filepath, "w") as f:
-        print(f"file: {f}")
-        json.dump(message, f)
-    # my_bucket = s3.Bucket('pinterest-data-40dba6ba-1777-41cd-82ea-0ca36620f0b4')
-    response = s3.upload_file(filepath, 
-                             pinterest_s3_bucket, 
-                             f"posts/{filename}")
+    pin_filename = f"pin_{message.value['index']}.json"
+    json_object = json.dumps(message.value)
+    my_bucket.put_object(Key=pin_filename, Body=json_object)
+    
+    # # define tmp filepath on local machine to store json object
+    # filename = f"pin_{message.value['index']}.json"
+    # filepath = f"/Users/agc/AiCore/pinterest-data-pipeline/tmp/{filename}"
+    # # convert message in batch consumer to json file
+    # with open(filepath, "w") as f:
+    #     print(f"file: {f}")
+    #     json.dump(message, f)
+    
+    # response = s3.upload_file(filepath, 
+    #                          pinterest_s3_bucket, 
+    #                          f"posts/{filename}")
     
     # result = s3.head_object(Bucket=pinterest_s3_bucket, Key=filepath)
     # print(result)
--- data_processing.py
+++ data_processing.py
@@ -1,28 +1,61 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
 import os
-import pyspark
 import yaml
 
-aws_credentials_file = "aws_config.yaml"
+aws_credentials_file = "/Users/agc/AiCore/pinterest-data-pipeline/aws_config.yaml"
+s3_pinterest_uri = "s3a://pinterest-data-40dba6ba-1777-41cd-82ea-0ca36620f0b4/posts"
 
-# read data from S3 bucket with Spark
-# set environment variables (looks up configs from Maven Repo)
-os.environ["PYSPARK_SUBMIT_ARGS"] = "--packages com.amazonaws:aws-java-sdk-s3:1.12.430,org.apache.hadoop:hadoop-aws:3.3.4 pyspark-shell"
-config = SparkConf().setAppName("S3_to_Spark").setMaster("local[*]")
 
-sc = SparkContext(conf=config)
-spark = SparkSession(sc).builder.appName("S3_to_Spark").master("local[*]").getOrCreate()
+def read_s3_to_spark():
+    # read data from S3 bucket with Spark
+    # set environment variables (looks up configs from Maven Repo)
+    os.environ["PYSPARK_SUBMIT_ARGS"] = "--packages com.amazonaws:aws-java-sdk-s3:1.12.430,org.apache.hadoop:hadoop-aws:3.3.4 pyspark-shell"
+    config = SparkConf().setAppName("S3_to_Spark").setMaster("local[*]")
 
-with open(aws_credentials_file, "r") as f:
-    aws_creds = yaml.safe_load(f)
-# set up hadoop configuration
-hadoopConf = sc._jsc.hadoopConfiguration()
-hadoopConf.set("fs.s3a.access.key", aws_creds["AWS_ACCESS_KEY"])
-hadoopConf.set("fs.s3a.secret.key", aws_creds["AWS_SECRET_KEY"])
-hadoopConf.set("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider")
+    sc = SparkContext(conf=config)
+    spark = SparkSession(sc).builder.appName("S3_to_Spark").master("local[*]").getOrCreate()
 
-# create spark dataframe to read in files from the S3 bucket
-while True:
-    df = spark.read.json(f"s3a://pinterest-data-40dba6ba-1777-41cd-82ea-0ca36620f0b4/posts/pin_10020.json")
-    df.show()
+    with open(aws_credentials_file, "r") as f:
+        aws_creds = yaml.safe_load(f)
+    
+    # set up hadoop configuration
+    hadoopConf = sc._jsc.hadoopConfiguration()
+    hadoopConf.set("fs.s3a.access.key", aws_creds["AWS_ACCESS_KEY"])
+    hadoopConf.set("fs.s3a.secret.key", aws_creds["AWS_SECRET_KEY"])
+    hadoopConf.set("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider")
+
+    # create spark dataframe to read in files from the S3 bucket
+    df = spark.read.json(s3_pinterest_uri)
+    # df.show(20)
+    
+    return df
+
+def preprocess_data(df):
+    df = df.withColumn("follower_count", when(df.follower_count.endswith("k"),regexp_replace(df.follower_count,"k","000")) \
+                                            .when(df.follower_count.endswith("M"),regexp_replace(df.follower_count,"M","000000")) \
+                                            .when(df.follower_count == "User Info Error", None)
+                                            .otherwise(df.follower_count)
+                                            )
+    df = df.withColumn("save_location", regexp_replace("save_location", "Local save in ", ""))
+    df = df.withColumn("title", when(df.title == "No Title Data Available", None).otherwise(df.title))
+    df = df.withColumn("description", when(df.description == "No description available Story format", None).otherwise(df.description))
+    df = df.withColumn("image_src", when(df.image_src == "Image src error.", None).otherwise(df.image_src))
+    df = df.withColumn("tag_list", when(df.tag_list == "N,o, ,T,a,g,s, ,A,v,a,i,l,a,b,l,e", None).otherwise(df.tag_list))
+    
+    df = df.withColumn("follower_count", df.follower_count.cast(LongType()))
+    
+    df_preprocessed = df.dropna("all", subset=["title", "description", "follower_count"])
+    
+    df_preprocessed.where(col("tag_list").isNull()).show()
+    df_preprocessed.printSchema()
+    
+
+    return df_preprocessed
+
+
+if __name__ == "__main__":
+    preprocess_data(read_s3_to_spark())
+
